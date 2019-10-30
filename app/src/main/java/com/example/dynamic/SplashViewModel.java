32ff759f0afb02b4b534f6db43f59a39f221bb15
Vinicius de Andrade Sauter <vsauter@brq.com>
2019-10-30 16:17:44 -0300

    Install Feature

--- app/src/main/java/com/example/dynamic/SplashViewModel.java
+++ app/src/main/java/com/example/dynamic/SplashViewModel.java
@@ -4,10 +4,8 @@ import android.app.Application;
 
 import androidx.annotation.NonNull;
 import androidx.features.DynamicViewModel;
-import androidx.features.FeatureProvider;
 import androidx.features.InstallStatus;
 import androidx.lifecycle.LiveData;
-import androidx.lifecycle.MediatorLiveData;
 import androidx.lifecycle.Transformations;
 
 import com.example.dynamic.login.LoginFeatureProvider;
@@ -15,12 +13,10 @@ import com.example.dynamic.payments.PaymentFlowFeature;
 
 @SuppressWarnings("WeakerAccess")
 public class SplashViewModel extends DynamicViewModel {
-    private FeatureProvider featureProvider = new FeatureProvider(this);
-
-    private MediatorLiveData<InstallStatus> loginModuleState;
-    private MediatorLiveData<InstallStatus> paymentsModuleState;
-    private MediatorLiveData<LoginFeatureProvider> loginFeatureProvider;
-    private MediatorLiveData<PaymentFlowFeature> paymentFlowFeature;
+    private LiveData<InstallStatus> loginModuleState;
+    private LiveData<InstallStatus> paymentsModuleState;
+    private LiveData<LoginFeatureProvider> loginFeatureProvider;
+    private LiveData<PaymentFlowFeature> paymentFlowFeature;
     private LiveData<Boolean> isAuthenticated;
 
     public SplashViewModel(@NonNull Application application) {
@@ -30,61 +26,28 @@ public class SplashViewModel extends DynamicViewModel {
 
     public LiveData<InstallStatus> getLoginModuleState() {
         if (loginModuleState == null) {
-            loginModuleState = new MediatorLiveData<>();
-//            if (isModuleInstalled("login")) {
-//                loginModuleState.setValue(InstallStatus.INSTALLED);
-//            } else
-            {
-                loginModuleState.addSource(getInstallStatus(), installStatus -> {
-                    if (installStatus.getModules().contains("login") && installStatus != InstallStatus.UNKNOWN) {
-                        loginModuleState.setValue(installStatus);
-                    }
-                });
-            }
+            loginModuleState = getInstallStatus("login");
         }
         return loginModuleState;
     }
 
     public LiveData<InstallStatus> getPaymentsModuleState() {
         if (paymentsModuleState == null) {
-            paymentsModuleState = new MediatorLiveData<>();
-//            //
-//            if (isModuleInstalled("payments")) {
-//                paymentsModuleState.setValue(InstallStatus.INSTALLED);
-//            } else
-            {
-                paymentsModuleState.addSource(getInstallStatus(), installStatus -> {
-                    if (installStatus.getModules().contains("payments") && installStatus != InstallStatus.UNKNOWN) {
-                        paymentsModuleState.setValue(installStatus);
-                    }
-                });
-            }
+            paymentsModuleState = getInstallStatus("payments");
         }
         return paymentsModuleState;
     }
 
     public LiveData<PaymentFlowFeature> getPaymentFlowFeature() {
         if (paymentFlowFeature == null) {
-            paymentFlowFeature = new MediatorLiveData<>();
-
-            paymentFlowFeature.addSource(installModules(PaymentFlowFeature.MODULE_NAME), installStatus -> {
-                if (paymentFlowFeature.getValue() == null && installStatus.isInstalled()) {
-                    paymentFlowFeature.setValue(featureProvider.get(PaymentFlowFeature.class));
-                }
-            });
+            paymentFlowFeature = installFeature(PaymentFlowFeature.MODULE_NAME, PaymentFlowFeature.class);
         }
         return paymentFlowFeature;
     }
 
     public LiveData<LoginFeatureProvider> getLoginFeatureProvider() {
         if (loginFeatureProvider == null) {
-            loginFeatureProvider = new MediatorLiveData<>();
-
-            loginFeatureProvider.addSource(installFeatures(LoginFeatureProvider.class), installStatus -> {
-                if (loginFeatureProvider.getValue() == null && installStatus.isInstalled()) {
-                    loginFeatureProvider.setValue(featureProvider.get(LoginFeatureProvider.class));
-                }
-            });
+            loginFeatureProvider = installFeature(LoginFeatureProvider.class);
         }
         return loginFeatureProvider;
     }
--- features/src/main/java/androidx/features/DynamicViewModel.java
+++ features/src/main/java/androidx/features/DynamicViewModel.java
@@ -5,6 +5,7 @@ import android.app.Application;
 import androidx.annotation.NonNull;
 import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MediatorLiveData;
 import androidx.lifecycle.MutableLiveData;
 
 import com.google.android.play.core.splitinstall.SplitInstallManager;
@@ -18,6 +19,7 @@ import java.util.Set;
 
 @SuppressWarnings({"unused", "WeakerAccess"})
 public class DynamicViewModel extends AndroidViewModel {
+    private FeatureProvider featureProvider = new FeatureProvider(this);
     private final SplitInstallManager splitInstallManager = SplitInstallManagerFactory.create(getApplication());
     private Integer sessionId = 0;
 
@@ -48,14 +50,93 @@ public class DynamicViewModel extends AndroidViewModel {
         return installStatus;
     }
 
+    public LiveData<InstallStatus> getInstallStatus(Class<?>... featureClasses) {
+        Set<String> modulesToInstall = getModulesFromFeatureClasses(featureClasses);
+        MediatorLiveData<InstallStatus> featureInstallStatus = new MediatorLiveData<>();
+        featureInstallStatus.addSource(installStatus, installStatus -> {
+            boolean isFromFeature = false;
+            for (String module : modulesToInstall) {
+                if (installStatus.getModules().contains(module) && installStatus != InstallStatus.UNKNOWN) {
+                    isFromFeature = true;
+                }
+            }
+            if (isFromFeature) {
+                featureInstallStatus.setValue(installStatus);
+            }
+        });
+        return featureInstallStatus;
+    }
+
+    public LiveData<InstallStatus> getInstallStatus(String... modules) {
+        MediatorLiveData<InstallStatus> featureInstallStatus = new MediatorLiveData<>();
+        featureInstallStatus.addSource(installStatus, installStatus -> {
+            boolean isFromFeature = false;
+            for (String module : modules) {
+                if (installStatus.getModules().contains(module) && installStatus != InstallStatus.UNKNOWN) {
+                    isFromFeature = true;
+                }
+            }
+            if (isFromFeature) {
+                featureInstallStatus.setValue(installStatus);
+            }
+        });
+        return featureInstallStatus;
+    }
+
     public LiveData<InstallStatus> installModules(String... modules) {
         Set<String> modulesToInstall = new HashSet<>(Arrays.asList(modules));
         startInstall(modulesToInstall);
-        return installStatus;
+        return getInstallStatus(modulesToInstall.toArray(new String[0]));
     }
 
     public LiveData<InstallStatus> installFeatures(Class<?>... featureClasses) {
-        Set<String> modulesToInstall = new HashSet<>();
+        Set<String> modulesToInstall = getModulesFromFeatureClasses(featureClasses);
+        startInstall(modulesToInstall);
+        return getInstallStatus(modulesToInstall.toArray(new String[0]));
+    }
+
+    public <T> LiveData<T> installFeature(Class<T> featureClass) {
+        MediatorLiveData<T> featureImpl = new MediatorLiveData<>();
+        FeatureFromModule featureFromModule = featureClass.getAnnotation(FeatureFromModule.class);
+        if (featureFromModule != null) {
+            String module = featureFromModule.value();
+            if (isModuleInstalled(module)) {
+                T feature = featureProvider.get(featureClass);
+                featureImpl.setValue(feature);
+            } else {
+                featureImpl.addSource(installFeatures(featureClass), installStatus -> {
+                    if (installStatus.isInstalled()) {
+                        T feature = featureProvider.get(featureClass);
+                        featureImpl.setValue(feature);
+                    }
+                });
+            }
+        } else {
+            try {
+                T feature = featureProvider.get(featureClass);
+                featureImpl.setValue(feature);
+            } catch (Throwable t) {
+                throw new IllegalArgumentException("Feature where not found. " +
+                        "Please use @FeatureFromModule (\"the_module_name\") to define which module this feature implemented." +
+                        "Or use installFeatures(String module, Class<T> featureClass) instead.", t);
+            }
+        }
+        return featureImpl;
+    }
+
+    public <T> LiveData<T> installFeature(String module, Class<T> featureClass) {
+        MediatorLiveData<T> featureImpl = new MediatorLiveData<>();
+        featureImpl.addSource(installModules(module), installStatus -> {
+            if (installStatus.isInstalled()) {
+                T feature = featureProvider.get(featureClass);
+                featureImpl.setValue(feature);
+            }
+        });
+        return featureImpl;
+    }
+
+    private Set<String> getModulesFromFeatureClasses(Class<?>... featureClasses) {
+        Set<String> modules = new HashSet<>();
         for (Class<?> featureClass : featureClasses) {
             FeatureFromModule featureFromModule = featureClass.getAnnotation(FeatureFromModule.class);
             if (featureFromModule == null) {
@@ -63,12 +144,10 @@ public class DynamicViewModel extends AndroidViewModel {
                         "Please use @FeatureFromModule (\"the_module_name\") to define which module this feature implemented.");
             } else {
                 String module = featureFromModule.value();
-                modulesToInstall.add(module);
-
+                modules.add(module);
             }
         }
-        startInstall(modulesToInstall);
-        return installStatus;
+        return modules;
     }
 
     private void startInstall(Set<String> modules) {
